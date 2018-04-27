8604bfdcd1f643ab76cc46f6acb6603ea3e2174c
Zvonkoi <zvonimir.petrovic@student.mev.hr>
2018-04-27 16:37:44 +0200

    Modeli

---
+++ Konzultacije/Konzultacije/Models/Upit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Konzultacije.Models
+{
+    public class Upit
+    {
+        public int Id_upit { get; set; }
+        public int Id_student { get; set; }
+        public int Id_profesor { get; set; }
+        public DateTime Datum { get; set; }
+        public string Naslov { get; set; }
+        public string Opis { get; set; }
+        public bool Odgovoren { get; set; }
+
+    }
+}
