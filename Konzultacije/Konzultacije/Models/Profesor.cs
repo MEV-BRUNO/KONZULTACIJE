3bf3e31f747def66b84ea6129d99f2bc026b3421
Igor Tomić <igro.tomic@mev.hr>
2018-05-16 14:29:06 +0200

    Login forma. Igor Tomić

---
+++ Konzultacije/Konzultacije/Models/Profesor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Konzultacije.Models
+{
+    public class Profesor
+    {
+       public int Id_profesor { get; set; }
+       public string Ime_I_Prezime { get; set; }
+       public string Email { get; set; }
+       public string Lozinka { get; set; }
+       public string Aktivacijski_Link { get; set; }
+       public bool Aktivan { get; set; }
+
+    }
+}
---
+++ Konzultacije/Konzultacije/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Konzultacije.Models
+{
+    public class Student
+    {
+
+        public int Id_student { get; set; }
+        public string Ime_I_Prezime { get; set; }
+        public int Id_studij { get; set; }
+        public string Email { get; set; }
+        public string Lozinka { get; set;}
+        public string Aktivacijski_link { get; set; }
+        public bool Aktivan { get; set; }
+
+    }
+}
